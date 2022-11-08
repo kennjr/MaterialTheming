82286e2efcbe75e38e46f8bc8cec127395f8bd94
kenedy <kenedy@myhechar.pro>
2022-11-08 21:17:33 +0300

    moved the MaterialTheme into CompositionLocalProvider

--- app/src/main/java/com/ramanie/materialtheming/ui/theme/Theme.kt
+++ app/src/main/java/com/ramanie/materialtheming/ui/theme/Theme.kt
@@ -9,6 +9,7 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
@@ -60,9 +61,15 @@ fun MaterialThemingTheme(
         }
     }
 
-    MaterialTheme(
-        colorScheme = colorScheme,
-        typography = Typography,
-        content = content
-    )
+    CompositionLocalProvider(
+        LocalSpacing provides Spacing(),
+        // we can pass as many custom theming classes as we'd like e.g one for elevation, corner-radius, etc
+//        LocalElevation provides Elevation()
+        ) {
+        MaterialTheme(
+            colorScheme = colorScheme,
+            typography = Typography,
+            content = content
+        )
+    }
 }
